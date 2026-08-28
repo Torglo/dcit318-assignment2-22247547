edc180960e833dde6502c54253be16b6d84d8893
EMMANUEL MENSAH TORGLO <emtorglo@st.ug.edu.gh>
2026-08-28 01:08:35 +0000

    Inheritance And Overriding

---
+++ InheritanceAndOverriding/Program.cs
@@ -0,0 +1,39 @@
+namespace InheritanceAndOverriding;
+
+public class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+public static class Program
+{
+    public static void Main()
+    {
+        Animal animal = new Animal();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        animal.MakeSound();
+        dog.MakeSound();
+        cat.MakeSound();
+    }
+}
